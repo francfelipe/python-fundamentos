6e8f049f4158c1a4ba69d5f2ce68f5ebdb6193e6
Felipe Francisco <fsfrancisco@gmail.com>
2024-03-23 15:31:58 -0300

    buid: add first library geometry

---
+++ plumbers/geometria/areas.py
@@ -0,0 +1,20 @@
+def area_quadrado(lado):
+    '''
+    Calcula a área de um quadrado.
+    Lado * Lado
+    '''
+    return lado * lado 
+
+def area_triangulo(base, altura):
+    '''
+    Calcula a área de um triângulo.
+    (base * altura) / 2
+    '''
+    return (base * altura) / 2
+
+def area_retangulo(base, altura):
+    '''
+    Calcula a área de um retângulo.
+    base * altura
+    '''
+    return base * altura
