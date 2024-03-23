024295261d6b219ac47e6c6f39b9b61fcae64943
Felipe Francisco <fsfrancisco@gmail.com>
2024-03-23 15:46:53 -0300

    buid: usando a nova bibiloteca

--- plumbers/geometria/areas.py
+++ plumbers/geometria/areas.py
@@ -1,18 +1,18 @@
-def area_quadrado(lado):
+def area_quadrado1(lado):
     '''
     Calcula a área de um quadrado.
     Lado * Lado
     '''
     return lado * lado 
 
-def area_triangulo(base, altura):
+def area_triangulo1(base, altura):
     '''
     Calcula a área de um triângulo.
     (base * altura) / 2
     '''
     return (base * altura) / 2
 
-def area_retangulo(base, altura):
+def area_retangulo1(base, altura):
     '''
     Calcula a área de um retângulo.
     base * altura
